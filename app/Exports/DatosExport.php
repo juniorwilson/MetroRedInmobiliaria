f48903984714f793fa1964a41c679a3cebbfba74
jhonatan fernandez <56049236+jhonatan11530@users.noreply.github.com>
2021-10-28 20:44:45 -0500

    ok

--- app/Exports/DatosExport.php
+++ app/Exports/DatosExport.php
@@ -5,7 +5,9 @@ namespace App\Exports;
 use App\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Illuminate\Support\Facades\DB;
-class DatosExport implements FromCollection
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class DatosExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,9 +15,28 @@ class DatosExport implements FromCollection
     public function headings(): array
     {
         return [
-            'Id',
-            'Nombre',
-            'Email',
+            'id',
+            'Dirrecion Inmueble',
+            'ID Inmueble',
+            'Servicio Prestado',
+            'Opcion Compra',
+            'Ubicacion Inmueble',
+            'Estado General',
+            'Acuerdo Valor de Venta',
+            'Oferta Inmueble',
+            'Referido Nombre',
+            'Referido Telefono',
+            'Referido Nombre2',
+            'Referido Telefono2',
+            'Nombre Cliente',
+            'Cedula Cliente',
+            'Telefono Cliente',
+            'Mail Cliente',
+            'Nombre Propietario',
+            'Cedula Propietario',
+            'Telefono Propietario',
+            'Mail Propietario',
+            'fecha'
         ];
     }
     public function collection()
--- app/Exports/UsersExport2.php
+++ app/Exports/UsersExport2.php
@@ -5,7 +5,9 @@ namespace App\Exports;
 use App\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Illuminate\Support\Facades\DB;
-class UsersExport2 implements FromCollection
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class UsersExport2 implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,14 +15,38 @@ class UsersExport2 implements FromCollection
     public function headings(): array
     {
         return [
-            'Id',
-            'Nombre',
-            'Email',
+            'id',
+            'Dirrecion Inmueble',
+            'ID Inmueble',
+            'Servicio Prestado',
+            'Opcion Compra',
+            'Ubicacion Inmueble',
+            'Estado General',
+            'Acuerdo Valor de Venta',
+            'Oferta Inmueble',
+            'Referido Nombre',
+            'Referido Telefono',
+            'Referido Nombre2',
+            'Referido Telefono2',
+            'Nombre Cliente',
+            'Cedula Cliente',
+            'Telefono Cliente',
+            'Mail Cliente',
+            'Nombre Propietario',
+            'Cedula Propietario',
+            'Telefono Propietario',
+            'Mail Propietario',
+            'fecha'
         ];
     }
     public function collection()
     {
-        $users = DB::table('formulario_encuesta_venta')->select('id','DirrecionInmueble', 'IDInmueble','ServicioPrestado','OpcionCompra','UbicacionInmueble','EstadoGeneral','AcuerdoValordeVenta','OfertaInmueble','ReferidoNombre','ReferidoTelefono','ReferidoNombre2','ReferidoTelefono2','NombreCliente','CedulaCliente','TelefonoCliente','MailCliente','NombrePropietario','CedulaPropietario','TelefonoPropietario','MailPropietario','created_at')->get();
+        $users = DB::table('formulario_encuesta_venta')
+        ->select('id','DirrecionInmueble', 'IDInmueble','ServicioPrestado','OpcionCompra',
+        'UbicacionInmueble','EstadoGeneral','AcuerdoValordeVenta','OfertaInmueble','ReferidoNombre',
+        'ReferidoTelefono','ReferidoNombre2','ReferidoTelefono2','NombreCliente','CedulaCliente',
+        'TelefonoCliente','MailCliente','NombrePropietario','CedulaPropietario','TelefonoPropietario'
+        ,'MailPropietario','created_at')->get();
         return $users;
     }
 }
